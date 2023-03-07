45580cfb5c543dff2d928d5fb70292b86f7de7e7
ADEMOLA ABIODUN SAHEED <77112906+Eminent01@users.noreply.github.com>
2023-03-07 22:16:13 +0000

    Update actor.py

--- model/ddpg/actor.py
+++ model/ddpg/actor.py
@@ -43,30 +43,30 @@ class ActorNetwork(object):
         # Actor Network
         self.inputs, self.out, self.scaled_out = self.create_actor_network()
 
-        self.network_params = tf.trainable_variables()
+        self.network_params = tf.compat.v1.trainable_variables()
 
         # Target Network
         self.target_inputs, self.target_out, self.target_scaled_out = self.create_actor_network()
 
-        self.target_network_params = tf.trainable_variables()[
+        self.target_network_params = tf.compat.v1.trainable_variables()[
                                      len(self.network_params):]
 
         # Op for periodically updating target network with online network weights
         self.update_target_network_params = \
-            [self.target_network_params[i].assign(tf.multiply(self.network_params[i], self.tau) +
-                                                  tf.multiply(self.target_network_params[i], 1. - self.tau))
+            [self.target_network_params[i].assign(tf.compat.v1.multiply(self.network_params[i], self.tau) +
+                                                  tf.compat.v1.multiply(self.target_network_params[i], 1. - self.tau))
              for i in range(len(self.target_network_params))]
 
         # This gradient will be provided by the critic network
-        self.action_gradient = tf.placeholder(tf.float32, [None] + self.a_dim)
+        self.action_gradient = tf.compat.v1.placeholder(tf.compat.v1.float32, [None] + self.a_dim)
 
         # Combine the gradients here
-        self.unnormalized_actor_gradients = tf.gradients(
+        self.unnormalized_actor_gradients = tf.compat.v1.gradients(
             self.scaled_out, self.network_params, -self.action_gradient)
-        self.actor_gradients = list(map(lambda x: tf.div(x, self.batch_size), self.unnormalized_actor_gradients))
+        self.actor_gradients = list(map(lambda x: tf.compat.v1.div(x, self.batch_size), self.unnormalized_actor_gradients))
 
         # Optimization Op
-        self.optimize = tf.train.AdamOptimizer(self.learning_rate). \
+        self.optimize = tf.compat.v1.train.AdamOptimizer(self.learning_rate). \
             apply_gradients(zip(self.actor_gradients, self.network_params))
 
         self.num_trainable_vars = len(self.network_params) + len(self.target_network_params)
