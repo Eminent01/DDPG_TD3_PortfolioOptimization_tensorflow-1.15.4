fbda66189ea7b7a1781be8cc00e9ca998e595c4f
ADEMOLA ABIODUN SAHEED <77112906+Eminent01@users.noreply.github.com>
2023-03-07 22:18:14 +0000

    Update critic.py

--- model/ddpg/critic.py
+++ model/ddpg/critic.py
@@ -25,26 +25,26 @@ class CriticNetwork(object):
         # Create the critic network
         self.inputs, self.action, self.out = self.create_critic_network()
 
-        self.network_params = tf.trainable_variables()[num_actor_vars:]
+        self.network_params = tf.compat.v1.trainable_variables()[num_actor_vars:]
 
         # Target Network
         self.target_inputs, self.target_action, self.target_out = self.create_critic_network()
 
-        self.target_network_params = tf.trainable_variables()[(len(self.network_params) + num_actor_vars):]
+        self.target_network_params = tf.compat.v1.trainable_variables()[(len(self.network_params) + num_actor_vars):]
 
         # Op for periodically updating target network with online network
         # weights with regularization
         self.update_target_network_params = \
-            [self.target_network_params[i].assign(tf.multiply(self.network_params[i], self.tau) \
-                                                  + tf.multiply(self.target_network_params[i], 1. - self.tau))
+            [self.target_network_params[i].assign(tf.compat.v1.multiply(self.network_params[i], self.tau) \
+                                                  + tf.compat.v1.multiply(self.target_network_params[i], 1. - self.tau))
              for i in range(len(self.target_network_params))]
 
         # Network target (y_i)
-        self.predicted_q_value = tf.placeholder(tf.float32, [None, 1])
+        self.predicted_q_value = tf.compat.v1.placeholder(tf.float32, [None, 1])
 
         # Define loss and optimization Op
         self.loss = tflearn.mean_square(self.predicted_q_value, self.out)
-        self.optimize = tf.train.AdamOptimizer(
+        self.optimize = tf.compat.v1.train.AdamOptimizer(
             self.learning_rate).minimize(self.loss)
 
         # Get the gradient of the net w.r.t. the action.
@@ -52,7 +52,7 @@ class CriticNetwork(object):
         # this will sum up the gradients of each critic output in the minibatch
         # w.r.t. that action. Each output is independent of all
         # actions except for one.
-        self.action_grads = tf.gradients(self.out, self.action)
+        self.action_grads = tf.compat.v1.gradients(self.out, self.action)
 
     def create_critic_network(self):
         raise NotImplementedError('Create critic should return (inputs, action, out)')
